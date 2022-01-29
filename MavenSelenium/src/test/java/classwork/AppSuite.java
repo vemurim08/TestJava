410f7cf07a96c19e2a4d4b131597b23ecb41ae04
madhavi <vemurim08@gmail.com>
2022-01-29 16:11:11 -0600

    first commit

---
+++ MavenSelenium/src/test/java/classwork/AppSuite.java
@@ -0,0 +1,44 @@
+package classwork;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AppSuite {
+public WebDriver driver;
+	
+	@Test(priority=1)
+	public void launchBrowser() {
+		System.setProperty("webdriver.chrome.driver","C:\\Madhavi\\software\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		//timeout
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofMinutes(3));
+		driver.manage().window().maximize();
+		driver.get("http://automationpractice.com");
+		String url = driver.getCurrentUrl();
+		String title = driver.getTitle();
+		System.out.println(url+"\t" +title);
+	}
+	@Test(priority=3)
+	public void navigateHome() {
+		driver.findElement(By.xpath("//img[@alt='My Store']")).click();
+	}
+	@Test(priority=2)
+	public void searchDress() {
+		String searchinput = "//input[@name='search_query']";
+		String searchBtn = "//button[@name='submit_search']";
+		driver.findElement(By.xpath(searchinput)).click();
+		driver.findElement(By.xpath(searchinput)).sendKeys("Summer dresses");
+		driver.findElement(By.xpath(searchBtn)).click();
+	}
+	
+	@Test(priority=4)
+	public void quitBrowser() {
+		driver.quit();
+	}
+	
+}
